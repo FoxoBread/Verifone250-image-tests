0727ff76b70b130267ea3d31f8755cd2c976fd7c
FoxoBread <foxobread@gmail.com>
2025-12-10 02:49:13 +0000

    Fix small mistakes
    Sometimes you think you fixed it, but in fact you did not.

--- main.py
+++ main.py
@@ -17,25 +17,25 @@ def loop(DATA): #{
 		CHUNK = DATA[i:i+6];									#Get 6 bits of bit array.
 		if len(CHUNK) < 6: CHUNK += [0] * (6 - len(CHUNK));					#If we're out of bits, pad with 0.
 		BIT_VALUE = 0x40;									#Start with 1000000.
-		for i, v in enumerate(CHUNK): #{							#Loop through the 6 bits in chunk.
-			if v: BIT_VALUE |= (1 << (5 - i));						#If bit is 1, change BIT_VALUE to 1XXXXX where X is whichever bits is to be set.
+		for _i, v in enumerate(CHUNK): #{							#Loop through the 6 bits in chunk.
+			if v: BIT_VALUE |= (1 << (5 - _i));						#If bit is 1, change BIT_VALUE to 1XXXXX where X is whichever bits is to be set.
 		#}											#End chunk loop.
 		Serial.write(bytes([BIT_VALUE]));							#Write either 1000000 if bit was 0 or 1XXXXX where X is bits of the image chunk.
 	#}
 #}
 
 def main(): #{
-	Serial.write(b'\x1B\x63');										#Reset printer.			0x1B, 0x63
-	Serial.write(b'\x1C');											#Enter native mode.		0x1C
-	Serial.write(b'\x1E');											#Enter double-width mode.	0x1E
-	Serial.write(b'\x1B\x67');										#Enter dot graphics mode.	0x1B, 0x67
+	Serial.write(b'\x1B\x63');									#Reset printer.			0x1B, 0x63
+	Serial.write(b'\x1C');										#Enter native mode.		0x1C
+	Serial.write(b'\x1E');										#Enter double-width mode.	0x1E
+	Serial.write(b'\x1B\x67');									#Enter dot graphics mode.	0x1B, 0x67
 
-	IMAGE	= Image.open(sys.argv[1]).convert('1').resize([420, 420]);				#Load image as 1 bit 420x420 graphic.
-
+	IMAGE		= Image.open(sys.argv[1]).convert('1').resize([420, 420]);			#Load image as 1 bit 420x420 graphic.
+	IMAGE_DATA	= list(IMAGE.getdata());							#Load raw image data.
 	for Y in range(IMAGE.height): #{								#Loop through each vertical line in image.
 		LINES	= [0] * 420;									#Initialise blank array for horizontal pixel.
 		for X in range(IMAGE.width): #{								#Loop through each horizontal pixel in image.
-			PIXEL_VALUE = list(IMAGE.getdata())[Y * IMAGE.width+X]				#Get pixel value for given X and Y.
+			PIXEL_VALUE = IMAGE_DATA[Y * IMAGE.width+X];					#Get pixel value for given X and Y.
 			if PIXEL_VALUE == 0: LINES[X] = 1;						#If pixel is black, set bit in array to 1 (black).
 			else: LINES[X] = 0;								#If pixel is white, set bit in array to 0 (white).
 		#}											#End horizontal loop.
